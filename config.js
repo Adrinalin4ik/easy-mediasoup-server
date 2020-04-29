5fa325b00c61b1ad41abe6fc3dd80c361e507296
Alexey Panfilkin <soild@mail.ru>
2020-04-29 19:33:44 +0300

    add telegram notifications

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 module.exports =
 {
 	// DEBUG env variable For the NPM debug module.
+	mediaserverName: process.env.mediaserverName || 'Mediasoup 1',
 	stage: process.env.stage || "beta",
 	serverPort: +process.env.serverPort || 3443,
 	debug: process.env.logLevel || '*INFO* *WARN* *ERROR*',
 	// Listening hostname for `gulp live|open`.
-	domain: 'localhost',
+	domain: process.env.domain || 'http://localhost',
 	basicAuth: {
 		username:process.env.basicAuthUsername || "admin",
 		password:process.env.basicAuthPassword || "password",
@@ -15,7 +16,16 @@ module.exports =
 		key : `${__dirname}/certs/mediasoup-demo.localhost.key.pem`
 	},
 	numWorkers: +process.env.numWorkers || 4,
-
+	telegram: {
+		enabled: process.env.telegramEnabled,
+		proxyHost: process.env.telegramProxyHost,
+		proxyPort: process.env.telegramProxyPort,
+		cpuNotificationTheshold: +process.env.cpuNotificationTheshold,
+		botToken: process.env.telegrambotToken,
+		chatId: process.env.telegramChatId,
+		warningLink: process.env.warningLink,
+		roomLink: "http://$roomId"
+	},
 	send_active_speakers_time_interval: +process.env.send_active_speakers_time_interval || 2000,
 	active_speaker_db_level: +process.env.active_speaker_db_level || -35,
 	active_enable_check_active_speakers: process.env.active_enable_check_active_speakers || true,
